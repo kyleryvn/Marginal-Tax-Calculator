d00b83a295389788af421a2207fb382d5a9634bf
Kyle <78947025+kyleryvn@users.noreply.github.com>
2022-03-11 15:37:32 -0500

    Delete TaxService.java

--- src/main/java/com/github/kyleryvn/taxservice/services/TaxService.java
+++
@@ -1,8 +0,0 @@
-package com.github.kyleryvn.taxservice.services;
-
-public class TaxService {
-
-    public static double getEffectiveRate(double totalTaxes, double income) {
-        return (totalTaxes / income) * 100;
-    }
-}
