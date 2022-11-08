8303acacb5ccffe001e834869bdd7074cb0a2800
Kyle <78947025+kyleryvn@users.noreply.github.com>
2022-11-08 17:20:16 -0500

    Added support for web scrapping

--- src/main/java/com/github/kyleryvn/taxservice/dao/ParseHTML.java
+++ src/main/java/com/github/kyleryvn/taxservice/dao/ParseHTML.java
@@ -1,2 +1,65 @@
-package com.github.kyleryvn.taxservice.dao;public class ParseHTML {
+package com.github.kyleryvn.taxservice.dao;
+
+import com.github.kyleryvn.taxservice.model.StateTaxRule;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParseHTML {
+
+    public static List<StateTaxRule> parseHtml(String state, String filingStatus) {
+        String url = "https://www.incometaxpro.net/tax-rates/" + state.toLowerCase() + ".htm";
+        List<StateTaxRule> stateRates = new ArrayList<>();
+        Elements data;
+
+        try {
+            Document document = Jsoup.connect(url).get();
+
+            data = switch (filingStatus) {
+                case "S" -> document.select("table.statebrackets:first-of-type");
+                case "MFJ" -> document.select("table.statebrackets:nth-of-type(2)");
+                case "MFS" -> document.select("table.statebrackets:nth-of-type(3)");
+                case "HH" -> document.select("table.statebrackets:last-of-type");
+                default -> throw new IllegalStateException("Unexpected value");
+            };
+
+            stateRates = extractData(filingStatus, data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return stateRates;
+    }
+
+    private static List<StateTaxRule> extractData(String filingStatus, Elements elements) {
+        List<StateTaxRule> stateData = new ArrayList<>();
+        Elements data = elements.select("tbody tr");
+
+        for (int i = 0; i < data.size(); i++) {
+            Element rate = data.select("td:eq(3)").get(i);
+            Element salaryRange1 = data.select("td:eq(0)").get(i);
+            Element salaryRange2 = data.select("td:eq(2)").get(i);
+
+            double taxRate = parseDouble(rate);
+            double salaryRangeOne = parseDouble(salaryRange1);
+            double salaryRangeTwo = salaryRange2.text().equalsIgnoreCase("∞") ? Double.POSITIVE_INFINITY
+                    : parseDouble(salaryRange2);
+
+            stateData.add(new StateTaxRule(taxRate, filingStatus, salaryRangeOne, salaryRangeTwo));
+        }
+
+        return stateData;
+    }
+
+    private static double parseDouble(Element element) {
+        if (element.text().contains("%"))
+            return Double.parseDouble(element.text().replace("%", "").strip()) / 100;
+
+        return Double.parseDouble(element.text().replaceAll("[,|$]", "").strip());
+    }
 }
