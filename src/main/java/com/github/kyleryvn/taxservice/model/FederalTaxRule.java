6468466e681219aa2d5f3d150b86388f5b0cb054
Kyle <78947025+kyleryvn@users.noreply.github.com>
2022-11-08 20:40:07 -0500

    Converted onjects to records

--- src/main/java/com/github/kyleryvn/taxservice/model/FederalTaxRule.java
+++ src/main/java/com/github/kyleryvn/taxservice/model/FederalTaxRule.java
@@ -1,33 +1,6 @@
 package com.github.kyleryvn.taxservice.model;
 
-public class FederalTaxRule {
-    private final double taxRate;
-    private final String filingStatus;
-    private final double salaryRangeOne;
-    private final double salaryRangeTwo;
-
-    public FederalTaxRule(double taxRate, String filingStatus, double salaryRangeOne, double salaryRangeTwo) {
-        this.taxRate = taxRate;
-        this.filingStatus = filingStatus;
-        this.salaryRangeOne = salaryRangeOne;
-        this.salaryRangeTwo = salaryRangeTwo;
-    }
-
-    public double getTaxRate() {
-        return taxRate;
-    }
-
-    public String getFilingStatus() {
-        return filingStatus;
-    }
-
-    public double getSalaryRangeOne() {
-        return salaryRangeOne;
-    }
-
-    public double getSalaryRangeTwo() {
-        return salaryRangeTwo;
-    }
+public record FederalTaxRule(double taxRate, String filingStatus, double salaryRangeOne, double salaryRangeTwo) {
 
     @Override
     public String toString() {
