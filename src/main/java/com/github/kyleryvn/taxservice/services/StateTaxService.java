cfc03d1f093445a8bdc16a5ffe3930adc7469f78
Kyle <78947025+kyleryvn@users.noreply.github.com>
2022-11-09 14:34:53 -0500

    Updated java docs

--- src/main/java/com/github/kyleryvn/taxservice/services/StateTaxService.java
+++ src/main/java/com/github/kyleryvn/taxservice/services/StateTaxService.java
@@ -7,13 +7,15 @@ import com.github.kyleryvn.taxservice.model.StateTaxRule;
 import java.util.*;
 import java.util.function.ToDoubleFunction;
 
+/**
+ *
+ */
 public class StateTaxService {
     private static Set<String> statesWithoutIncomeTax = new HashSet<>();
 
     public static double getStateTaxDue(String state, String filingStatus, double income) {
         List<StateTaxRule> stateTaxRules;
         setStatesWithoutIncomeTax();
-
 
         ToDoubleFunction<StateTaxRule> map = taxRule -> {
             double rangeTwo = Math.min(taxRule.salaryRangeTwo(), income);
@@ -22,7 +24,6 @@ public class StateTaxService {
 
         if (!statesWithoutIncomeTax.contains(state)) {
             stateTaxRules = ParseHTML.parseHtml(state, filingStatus);
-            //System.out.println(stateTaxRules);
 
             return stateTaxRules.stream()
                     .filter(taxRule -> taxRule.filingStatus().equalsIgnoreCase(filingStatus))
@@ -44,12 +45,33 @@ public class StateTaxService {
         }
     }
 
+    /**
+     * <p>
+     *     Calculates the taxpayer's effective rate.
+     * </p>
+     * <p>
+     *     A taxpayer’s tax bracket does not necessarily reflect the percentage of their income that they will pay in
+     *     taxes; the term for this is the effective tax rate. The term effective tax rate refers to the percent of income
+     *     that an individual or corporation owes/pays in taxes. The effective tax rate for individuals is the average
+     *     rate at which their earned income, such as wages, and unearned income, such as stock dividends, are taxed.
+     *     The effective tax rate for a corporation is the average rate at which its pre-tax profits are taxed, while
+     *     the statutory tax rate is the legal percentage established by law.
+     * </p>
+     * @param totalTaxes Taxpayer's total taxes due
+     * @param income Taxpayer's gross annual income
+     * @return Taxpayer's effective rate
+     */
     public static double getStateEffectiveRate(double totalTaxes, double income) {
         return (totalTaxes / income) * 100;
     }
 
-    // Alaska, Florida, Nevada, South Dakota, Texas, Tennessee,
-    // Washington, and Wyoming do not collect income taxes
+    /**
+     * <p>
+     *     This method grabs the states that collect no income tax by calling {@link StateDAO#getStates() StateDAO.getStates()}
+     *     and stores result in the {@link Set} {@code statesWithoutIncomeTax}. Alaska, Florida, Nevada, South Dakota, Texas,
+     *     Tennessee, Washington, and Wyoming do not collect income tax.
+     * </p>
+     */
     private static void setStatesWithoutIncomeTax() {
         statesWithoutIncomeTax = StateDAO.getStates();
     }
