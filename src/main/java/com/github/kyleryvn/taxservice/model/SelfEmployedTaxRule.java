a176d34b7ffed25694ef660470a2955fd17cca93
Kyle <78947025+kyleryvn@users.noreply.github.com>
2022-11-09 01:45:55 -0500

    Updated java doc

--- src/main/java/com/github/kyleryvn/taxservice/model/SelfEmployedTaxRule.java
+++ src/main/java/com/github/kyleryvn/taxservice/model/SelfEmployedTaxRule.java
@@ -5,6 +5,10 @@ package com.github.kyleryvn.taxservice.model;
  *     Represents a federal sel-employed tax bracket.
  * </p>
  * <p>
+ *     A self-employed person does not work for a specific employer who pays them a consistent salary or wage.
+ *     Self-employed individuals, or independent contractors, earn income by contracting with a trade or business directly.
+ * </p>
+ * <p>
  *     The term self-employment tax refers to taxes self-employed individuals and small business owners pay to the
  *     federal government to fund Medicare and Social Security. This tax is due when an individual has net earnings of $400
  *     or more in self-employment income over the course of the tax year or $108.28 or more from a tax-exempt church.
