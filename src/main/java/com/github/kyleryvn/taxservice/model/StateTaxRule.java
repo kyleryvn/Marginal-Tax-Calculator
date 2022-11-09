75426c50b7d0bbbd08852ff5baee7c105287235e
Kyle <78947025+kyleryvn@users.noreply.github.com>
2022-11-09 01:03:17 -0500

    Added java doc for StateTaxRule

--- src/main/java/com/github/kyleryvn/taxservice/model/StateTaxRule.java
+++ src/main/java/com/github/kyleryvn/taxservice/model/StateTaxRule.java
@@ -1,5 +1,14 @@
 package com.github.kyleryvn.taxservice.model;
 
+/**
+ * <p>
+ *     Represents a state tax bracket.
+ * </p>
+ * @param taxRate Rate for corresponding tax bracket
+ * @param filingStatus Individual's filing status
+ * @param salaryRangeOne Minimum value of salary range
+ * @param salaryRangeTwo Maximum value of salary range
+ */
 public record StateTaxRule(double taxRate, String filingStatus, double salaryRangeOne, double salaryRangeTwo) {
 
     @Override
