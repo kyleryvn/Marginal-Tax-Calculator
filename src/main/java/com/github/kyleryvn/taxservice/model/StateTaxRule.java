c9078a7f2644b9a641001c75088fa681ada3bb2c
Kyle <78947025+kyleryvn@users.noreply.github.com>
2022-11-09 01:27:17 -0500

    Updated java doc

--- src/main/java/com/github/kyleryvn/taxservice/model/StateTaxRule.java
+++ src/main/java/com/github/kyleryvn/taxservice/model/StateTaxRule.java
@@ -4,7 +4,11 @@ package com.github.kyleryvn.taxservice.model;
  * <p>
  *     Represents a state tax bracket.
  * </p>
- * @param taxRate Rate for corresponding tax bracket
+ * <p>
+ *     Tax brackets consist of several salary ranges, each with a corresponding tax rate to be applied to that range. For example,
+ *     Arizona has 4 tax brackets for each filing status, and
+ * </p>
+ * @param taxRate Rate for corresponding tax bracket. Will be converted to decimal from percent.
  * @param filingStatus Individual's filing status
  * @param salaryRangeOne Minimum value of salary range
  * @param salaryRangeTwo Maximum value of salary range
