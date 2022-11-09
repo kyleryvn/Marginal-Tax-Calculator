6f585fb00180fc858bf13e496b3541c01e2c9d07
Kyle <78947025+kyleryvn@users.noreply.github.com>
2022-11-08 20:19:25 -0500

    Updated javadoc for ParseHTML

--- src/main/java/com/github/kyleryvn/taxservice/dao/ParseHTML.java
+++ src/main/java/com/github/kyleryvn/taxservice/dao/ParseHTML.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class ParseHTML {
 
     /**
+     * <h3>Parse HTML</h3>
      * <p>
      *     Retrieves state's tax data by filtering filing status through the tables provided
      *     on the webpage. Valid filing statuses are "S" for Single, "MFJ" for Married Filing Jointly,
