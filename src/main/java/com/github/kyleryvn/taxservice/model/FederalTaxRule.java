4109bcd8f5adc8a24e0c95cdf2991d43c8bcf5f0
Kyle <78947025+kyleryvn@users.noreply.github.com>
2022-11-09 01:37:10 -0500

    Added java doc for FederalTacRule

--- src/main/java/com/github/kyleryvn/taxservice/model/FederalTaxRule.java
+++ src/main/java/com/github/kyleryvn/taxservice/model/FederalTaxRule.java
@@ -1,5 +1,23 @@
 package com.github.kyleryvn.taxservice.model;
 
+/**
+ * <p>
+ *     This record holds the blueprint for a federal tax bracket.
+ * </p>
+ * <p>
+ *     A tax bracket refers to a range of incomes subject to a certain income tax rate. Tax brackets result in a progressive
+ *     tax system, in which taxation progressively increases as an individual’s income grows. Low incomes fall into tax
+ *     brackets with relatively low income tax rates, while higher earnings fall into brackets with higher rates.
+ * </p>
+ * @param taxRate Corresponding tax rate for salary range.
+ * @param filingStatus Individual's filing status. Valid entries are "S", "MFJ", "MFS", "HH"
+ * @param salaryRangeOne Minimum value in salary range.
+ * @param salaryRangeTwo Maximum value in salary range.
+ *
+ * @author Kyle Schoenhardt
+ * @version 1.0.0
+ * @since 2022-01-12
+ */
 public record FederalTaxRule(double taxRate, String filingStatus, double salaryRangeOne, double salaryRangeTwo) {
 
     @Override
